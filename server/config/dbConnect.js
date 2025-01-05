3d2998e14e02fbb194e2d952c857bf757bb82584
Abdalrahman <aalghefa@uwo.ca>
2025-01-05 18:42:43 +0300

    added hybrid mongoose connection for User schema and Client connection for destinations collection for MongoDB connection

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,12 +1,29 @@
 const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://test:test@myapp.vvmry.mongodb.net/?retryWrites=true&w=majority&appName=Myapp";
-const client = new MongoClient(MONGO_URI);
+const client = new MongoClient(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
 
 async function dbConnect() {
     try {
+        // Connect MongoClient
         await client.connect();
-        console.log("Connected to MongoDB");
+
+        console.log("!!!--------------CONNECTED to MongoDB with MongoClient--------------!!!");
+
+        // Connect Mongoose
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log("!!!--------------CONNECTED to MongoDB with Mongoose-------------!!!");
+
     } catch (error) {
         console.error("Failed to connect to MongoDB:", error);
         process.exit(1);
